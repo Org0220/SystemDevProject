3b8997297f0a11ef0e3f88881fea5e06c81cd996
unknown <gevorgmarkarov@gmail.com>
2022-04-14 13:25:34 -0400

    database and controllers layout

--- db/users.sql
+++ db/users.sql
@@ -12,93 +12,69 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `users`
---
-CREATE DATABASE IF NOT EXISTS `users` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `users`;
 
--- --------------------------------------------------------
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` text NOT NULL,
+  `pass_hash` text NOT NULL,
+  `name` text NOT NULL,
+  `phone_number` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
---
--- Table structure for table `credentials`
---
 
-CREATE TABLE `credentials` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `pass_hash` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `date` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
---
--- Dumping data for table `credentials`
---
+CREATE TABLE `availabilities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` text NOT NULL,
+  `start` text NOT NULL,
+  `end` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
-INSERT INTO `credentials` (`id`, `username`, `pass_hash`) VALUES
-(1, 'test', '$2y$10$wIDezbUb3QiuuRAC1FKvp.GhZCWz5.yJ7c6wMygPY9yTbPe8nJYpe'),
-(2, 'parth', '$2y$10$KscysJLnoQHNL0vwLHvCY.n0kOFyDGU6DF0MuUG5aeK6AJBb8dsuK');
+CREATE TABLE `service` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `price` double NOT NULL,
+  `duration` text NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
--- --------------------------------------------------------
+CREATE TABLE `shampoo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `price` text NOT NULL,
+  `description` text NOT NULL,
+  `image_url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
---
--- Table structure for table `users`
---
+CREATE TABLE `show_case` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `image_url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `City` varchar(255) NOT NULL,
-  `Phone` int(10) NOT NULL,
-  `Picture` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`ID`, `Name`, `City`, `Phone`, `Picture`) VALUES
-(1, 'Daves', 'Halifax', 1, '621524ce40f00.jpg'),
-(54, 'NewUser', 'NewCity', 1, '621523556cf5e.jpg');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `credentials`
---
-ALTER TABLE `credentials`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `credentials`
---
-ALTER TABLE `credentials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` text NOT NULL,
+  `time` text NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `appointment_ibfk_1` (`client_id`),
+  KEY `service_id` (`service_id`),
+  CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
